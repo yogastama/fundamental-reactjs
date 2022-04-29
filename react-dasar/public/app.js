73bfe43f9a2b7ee6790759bab410ef904b7e5d61
yogastama <yogabagas69@gmail.com>
2022-04-29 11:11:16 +0700

    NEW - react list with map

--- react-dasar/public/app.js
+++ react-dasar/public/app.js
@@ -1,15 +1,10 @@
 function App() {
-  const [login, setLogin] = React.useState(false);
-  const titleAppRef = React.useRef(null);
-  React.useEffect(function () {
-    console.log('Hello');
-    titleAppRef.current.textContent = 'Login App';
-  }, []);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
-    ref: titleAppRef
-  }, "Simple Login Application"), /*#__PURE__*/React.createElement("p", null, login ? 'Udah login' : 'Belum login'), /*#__PURE__*/React.createElement("button", {
-    onClick: () => login ? setLogin(false) : setLogin(true)
-  }, login ? 'Logout' : 'Login'));
+  const fruits = ['Apple', 'Orange', 'Grape'];
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("ul", null, fruits.map(function (fruit) {
+    return /*#__PURE__*/React.createElement("li", {
+      key: fruit
+    }, fruit);
+  })));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.querySelector('#root'));
--- react-dasar/src/app.js
+++ react-dasar/src/app.js
@@ -1,21 +1,18 @@
 function App(){
-    const [login, setLogin] = React.useState(false);
-    const titleAppRef = React.useRef(null);
-    React.useEffect(function(){
-        console.log('Hello');
-        titleAppRef.current.textContent = 'Login App';
-    }, []);
+    const fruits = [
+        'Apple',
+        'Orange',
+        'Grape'
+    ];
     return (
         <>
-            <h1 ref={titleAppRef}>
-                Simple Login Application
-            </h1>
-            <p>
-                {login ? 'Udah login' : 'Belum login'}
-            </p>
-            <button onClick={() => login ? setLogin(false) : setLogin(true)}>
-                {login ? 'Logout' : 'Login'}
-            </button>
+            <ul>
+                {
+                    fruits.map(function(fruit){
+                        return <li key={fruit}>{fruit}</li>
+                    })
+                }
+            </ul>
         </>
     )
 }
